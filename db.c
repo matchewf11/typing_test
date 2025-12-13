05c2be9eabdcb8d1fde3d1b2b21e15bc771398e8
Matthew Franchini <matchewf11@gmail.com>
2025-12-12 18:32:35 -0800

    updated db.c for new ideas

--- db.c
+++ db.c
@@ -1,50 +1,87 @@
 #include <sqlite3.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// // -1 if there is an err
-// static int clear_phrases(sqlite3 *db) {
-//   char *sql = "DELETE FROM phrases;";
-//   char *err = NULL;
-//
-//   if (sqlite3_exec(db, sql, NULL, NULL, &err) != SQLITE_OK) {
-//     perror("could not delete all phrases");
-//     sqlite3_free(err);
-//     return -1;
-//   }
-//
-//   return 0;
-// }
-//
-// static int add_all_phrases() {
-//   return 0;
-// }
-//
-// // if it throws handle it better (return a default one)
-// // could throw if it can not open the db
-// void init_db() {
-//   sqlite3 *db;
-//   char *err_msg = NULL;
-//
-//   if (sqlite3_open("typing.db", &db) != SQLITE_OK) {
-//     perror("Could not open db");
-//     exit(1);
-//   }
-//   char *sql = "CREATE TABLE IF NOT EXISTS phrases ("
-//               "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-//               "phrase TEXT"
-//               ");";
-//
-//   // makea table with results
-//   // id of phrase used
-//   // cps
-//   // accuracy
-//
-//   int rc = sqlite3_exec(db, sql, NULL, NULL, &err_msg);
-//   if (rc != SQLITE_OK) {
-//     perror("Could not create tables");
-//     sqlite3_free(err_msg);
-//   }
-//
-//   // delete everything and reinsert it? for teh phrases
-//
-//   sqlite3_close(db);
-// }
+// return -1 for error
+// else return 0
+static int insert_default_phrases(sqlite3* db) {
+  return 0;
+}
+
+
+// return -1 for error
+static int is_phrases_empty(sqlite3 *db) {
+  sqlite3_stmt *stmt;
+  char *sql = "SELECT EXISTS (SELECT 1 FROM phrases);";
+  int exists = 0;
+
+  int rs = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+  if (rs != SQLITE_OK) {
+    return -1;
+  }
+
+  rs = sqlite3_step(stmt);
+  if (rs == SQLITE_ROW) {
+    exists = sqlite3_column_int(stmt, 0);
+  }
+  
+  sqlite3_finalize(stmt);
+
+  return exists == 0;
+}
+
+// make db and the tables if they do not exist
+// make sure that the db is not null
+// in client
+// make sure to free the db after called
+sqlite3 *build_db() {
+  sqlite3 *db;
+  char *err_msg = NULL;
+
+  int rs = sqlite3_open("typeing.db", &db);
+  if (rs != SQLITE_OK) {
+    return NULL;
+  }
+
+  char *pragma = "PRAGMA foreign_keys = ON;";
+  char *sql_phrases = "CREATE TABLE IF NOT EXISTS phrases ("
+                      "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                      "phrase TEXT"
+                      ");";
+  char *sql_results = "CREATE TABLE IF NOT EXISTS results("
+                      "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                      "accuracy REAL,"
+                      "cps REAL,"
+                      "phrase_id INTEGER"
+                      ");";
+
+  rs = sqlite3_exec(db, pragma, NULL, NULL, &err_msg);
+  if (rs != SQLITE_OK) {
+    perror(err_msg);
+    sqlite3_free(err_msg);
+    sqlite3_close(db);
+    return NULL;
+  }
+
+  rs = sqlite3_exec(db, sql_phrases, NULL, NULL, &err_msg);
+  if (rs != SQLITE_OK) {
+    perror(err_msg);
+    sqlite3_free(err_msg);
+    sqlite3_close(db);
+    return NULL;
+  }
+
+  rs = sqlite3_exec(db, sql_results, NULL, NULL, &err_msg);
+  if (rs != SQLITE_OK) {
+    perror(err_msg);
+    sqlite3_free(err_msg);
+    sqlite3_close(db);
+    return NULL;
+  }
+
+  // TODO: fill in teh phrases if they are not there
+  // call the is empty
+  // call the insert default phrases
+
+  return db;
+}
