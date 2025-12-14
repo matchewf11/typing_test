731586b490432541483c30c282610d5ae9ca580f
Matthew Franchini <matchewf11@gmail.com>
2025-12-13 16:23:39 -0800

    really basic parser implemented

--- cli.c
+++ cli.c
@@ -1,9 +1,45 @@
+#include <string.h>
+
+#include "cli.h"
+
+CliOptions parse_cli(char **args, int len) {
+  if (len == 1) {
+    return (CliOptions){
+        .cli_cmd = ERROR,
+        .err_msg = "too little arguments. Options: 'help', 'run', 'stats'",
+    };
+  } else if (len > 2) {
+    return (CliOptions){
+        .cli_cmd = ERROR,
+        .err_msg = "too many arguments, currently only support one arg",
+    };
+  }
+  if (strcmp(args[1], "help")) {
+    return (CliOptions){
+        .cli_cmd = HELP,
+        .err_msg = NULL,
+    };
+  } else if (strcmp(args[1], "run")) {
+    return (CliOptions){
+        .cli_cmd = RUN,
+        .err_msg = NULL,
+    };
+  } else if (strcmp(args[1], "stats")) {
+    return (CliOptions){
+        .cli_cmd = STATS,
+        .err_msg = NULL,
+    };
+  } else {
+    return (CliOptions){
+        .cli_cmd = ERROR,
+        .err_msg = "could not recognize args. Options: 'help', 'run', 'stats'",
+    };
+  }
+}
 
 // typedef enum {
 //   EXECUTABLE,
 // } CliTokens;
-
-void parse_cli(char **args, int len) {}
 
 // typedef enum { HELP, RUN, STATS } CliCmds;
 //
--- cli.h
+++ cli.h
@@ -1,4 +1,14 @@
 #ifndef CLI_H
 #define CLI_H
 
+typedef enum { ERROR, HELP, RUN, STATS } CliCmd;
+
+typedef struct {
+  CliCmd cli_cmd;
+  char *err_msg;
+} CliOptions;
+
+// no need to free err_msg
+CliOptions parse_cli(char **args, int len);
+
 #endif
