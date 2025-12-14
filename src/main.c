379382094f400e4d78ad639c29316997cf119b25
Matthew Franchini <matchewf11@gmail.com>
2025-12-14 10:55:17 -0800

    added basic cli to main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello\n");
+#include "cli/cli.h"
+
+int main(int argc, char *argv[]) {
+
+  CliCmd cmd;
+  ParseResult ps = parse_args(argc, argv, &cmd);
+
+  switch (cmd) {
+  case CMD_RUN:
+    printf("RUNNING\n");
+    break;
+  case CMD_HELP:
+    printf("HELPING\n");
+    break;
+  case CMD_STATS:
+    printf("STATING\n");
+    break;
+  case CMD_ERROR:
+    printf("ERRORING\n");
+    break;
+  }
+
   return 0;
 }
