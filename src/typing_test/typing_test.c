2001a71ca469eb22ee94575dbc77366b7c91a84e
Matthew Franchini <matchewf11@gmail.com>
2025-12-17 21:34:46 -0800

    changed comment location

--- src/typing_test/typing_test.c
+++ src/typing_test/typing_test.c
@@ -1,80 +1,72 @@
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
+#include "typing_test.h"
 #include "typing_token/typing_token.h"
 
-typedef struct {
+struct test_info {
   float accuracy;
   float time;
   float cps;
-} TestInfo;
+};
 
-TestInfo typing_test(const char *const *str_list, int len) {
+int typing_test(const char *const *str_list, int len, TestInfo *out) {
 
   int token_len;
   TypingToken *token_list = typing_token_list(str_list, len, &token_len);
   if (token_list == NULL) {
-    // handle this case
+    return -1; // error
   }
-
-  // set start and end
 
   struct timespec start, end;
 
-  //   int letter_i = 0;
-  //   while (letter_i < letter_len) {
-  //     clear_term();
-  //     print_input_letter(letter_list, letter_len);
-  //     char c = '\0';
-  //     if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
-  //       perror("could not read the thing");
-  //       exit(1);
-  //     } else if (c == '\0') {
-  //       continue;
-  //     }
-  //     if (letter_i == 0) {
-  //       clock_gettime(CLOCK_MONOTONIC, &start);
-  //     }
-  //     letter_set_status(letter_list, letter_i, c);
-  //     letter_i++;
-  //   }
-  //
-  //   clock_gettime(CLOCK_MONOTONIC, &end);
-  //   clear_term();
-  //
-  //   double acc = letter_accuracy(letter_list, letter_len);
-  //   double time_taken =
-  //       (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
-  //   double cps = letter_len / time_taken;
+  int letter_i = 0;
+  while (letter_i < token_len) {
+
+    // clear_term();
+
+    print_typing_token_list(token_list, token_len);
+
+    char c = '\0';
+
+    int err_code = read(STDIN_FILENO, &c, 1);
+    if (err_code == -1 && errno != EAGAIN) {
+      return -1; // error
+    } else if (c == '\0') {
+      continue;
+    }
+
+    if (letter_i == 0) {
+      clock_gettime(CLOCK_MONOTONIC, &start);
+    }
+
+    update_typing_token_status(token_list, letter_i, c);
+    letter_i++;
+  }
+
+  clock_gettime(CLOCK_MONOTONIC, &end);
+
+  // clear_term(); // do i need this?
+
+  float acc = letter_accuracy(token_list, token_len);
+  float time_taken =
+      (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+  float cps = token_len / time_taken;
 
   free(token_list);
 
-  // return (TestInfo){.accuracy = acc, .time = time_taken, .cps = cps};
-  return (TestInfo){};
+  out->accuracy = acc;
+  out->time = time_taken;
+  out->cps = cps;
+
+  return 0;
 }
 
-// typing.c
-// ```C
-// #include <errno.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <time.h>
-// #include <unistd.h>
-// ```
-//
-// typing.h
-// ```C
-// #ifndef TYPING_H
-// #define TYPING_H
-//
-// #include <stdio.h>
-//
-// TestInfo start_typing_test(char **str_list, int len);
-//
-// static inline void print_test_info(TestInfo test_info) {
-//   printf("Accuracy: %f\r\n", test_info.accuracy);
-//   printf("Time: %f seconds\r\n", test_info.time);
-//   printf("CPS: %f\r\n", test_info.cps);
-// }
-//
-// #endif
-// ```
+inline void print_test_info(const TestInfo test_info) {
+  printf("Accuracy: %f\r\n", test_info.accuracy);
+  printf("Time: %f seconds\r\n", test_info.time);
+  printf("CPS: %f\r\n", test_info.cps);
+}
--- src/typing_test/typing_test.h
+++ src/typing_test/typing_test.h
@@ -1,4 +1,11 @@
 #ifndef TYPING_TEST_H
 #define TYPING_TEST_H
 
+typedef struct test_info TestInfo;
+
+// return -1 on error, 0 on success
+int typing_test(const char *const *str_list, int len, TestInfo *out);
+
+inline void print_test_info(const TestInfo test_info);
+
 #endif
